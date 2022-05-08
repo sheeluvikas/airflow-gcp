190b6011fc8b3ed46dc807daa58edcebf78f384e
Vikas Katiyar <vikas@Vikass-MacBook-Air.local>
2022-05-08 10:02:58 +0530

    syntax error fix, working in GCP

--- dags/airflow-dataproc.py
+++ dags/airflow-dataproc.py
@@ -1,11 +1,9 @@
-
 from airflow import DAG
 from datetime import datetime
-from airflow.providers.goo
-gle.cloud.operators.dataproc import DataprocCreateClusterOperator, ClusterGenerator, DataprocDeleteClusterOperator, DataprocSubmitJobOperator
+from airflow.providers.google.cloud.operators.dataproc import DataprocCreateClusterOperator, ClusterGenerator, \
+    DataprocDeleteClusterOperator, DataprocSubmitJobOperator
 
-
-PROJECT_ID = "single-nebula-319205"
+PROJECT_ID = "imposing-ace-344215"
 CLUSTER_NAME = "cluster-6bd4"
 REGION = "us-central1"
 ZONE = 'us-central1-c'
@@ -22,21 +20,20 @@ SPARK_JOB = {
 }
 
 dag = DAG("airflow-dataproc", start_date=datetime(2021, 1, 1),
-    schedule_interval="@daily", catchup=False)
-
+          schedule_interval="@daily", catchup=False)
 
 config = ClusterGenerator(
-        project_id="single-nebula-319205",
-        num_masters=1,
-        num_workers=0,
-        master_machine_type='n1-standard-2',
-        worker_machine_type='n1-standard-2',
-        zone='us-central1-c',
-    ).make()
+    project_id="imposing-ace-344215",
+    num_masters=1,
+    num_workers=0,
+    master_machine_type='n1-standard-2',
+    worker_machine_type='n1-standard-2',
+    zone='us-central1-c',
+).make()
 
 create_cluster = DataprocCreateClusterOperator(
     task_id="create_cluster",
-    project_id="single-nebula-319205",
+    project_id="imposing-ace-344215",
     cluster_config=config,
     region="us-central1",
     cluster_name="cluster-6bd4",
@@ -44,12 +41,12 @@ create_cluster = DataprocCreateClusterOperator(
 )
 
 spark_task = DataprocSubmitJobOperator(
-        task_id="spark_task", job=SPARK_JOB, location=REGION, project_id=PROJECT_ID
+    task_id="spark_task", job=SPARK_JOB, location=REGION, project_id=PROJECT_ID
 )
 
 delete_cluster = DataprocDeleteClusterOperator(
     task_id="delete_cluster",
-    project_id="single-nebula-319205",
+    project_id="imposing-ace-344215",
     cluster_config=config,
     region="us-central1",
     cluster_name="cluster-6bd4",
@@ -58,5 +55,5 @@ delete_cluster = DataprocDeleteClusterOperator(
 
 create_cluster >> spark_task >> delete_cluster
 
-if __name__=="__main__":
+if __name__ == "__main__":
     dag.cli()
