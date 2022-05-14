3453e8b4990386cab4bb2dc5710ccd267b8c3ba9
Vikas Katiyar <vikas@Vikass-MacBook-Air.local>
2022-05-14 16:37:50 +0530

    dynamic dag with Variables airflow, working locally

--- dags/my_dag.py
+++ dags/my_dag.py
@@ -1,61 +1,62 @@
+""" This dag shows how to create a simple pipeline which uses BashOperator,
+first task copies the data from one location to another, and another task, which checks if the file has recevied at the location.
+if yes, then pass, otherwise fail"""
+
 from airflow import DAG
-from airflow.operators.python import PythonOperator, BranchPythonOperator
+from airflow.sensors.filesystem import FileSensor
 from airflow.operators.bash import BashOperator
+from airflow.models import Variable
 
-from random import randint
-from datetime import datetime
+from datetime import datetime, timedelta
 
-def _choose_best_model(ti):
-    accuracies = ti.xcom_pull(task_ids=[
-        'training_model_A',
-        'training_model_B',
-        'training_model_C'
-    ])
-    best_accuracy = max(accuracies)
-    if (best_accuracy > 8):
-        return 'accurate'
-    return 'inaccurate'
+EXEC_DATE = '{{ macros.ds_format(ds, "%Y-%m-%d", "%Y%m%d") }}'
 
 
-def _training_model():
-    return randint(1, 10)
-
-with DAG("my_dag", start_date=datetime(2021, 1, 1),
-    schedule_interval="@daily", catchup=False) as dag:
-
-        training_model_A = PythonOperator(
-            task_id="training_model_A",
-            python_callable=_training_model
+def create_dag(
+        dag_ids,
+        source_system_id):
+    dag = DAG(dag_ids,
+              start_date=datetime(2022, 5, 1),
+              schedule_interval="@daily")
+    with dag:
+        check_file_task = FileSensor(
+            task_id="check_file",
+            poke_interval=5,
+            filepath="/Users/vikas/app/tmp/abc.txt",
+            timeout=550,
+            dag=dag
         )
 
-        training_model_B = PythonOperator(
-            task_id="training_model_B",
-            python_callable=_training_model
-        )
-
-        training_model_C = PythonOperator(
-            task_id="training_model_C",
-            python_callable=_training_model
-        )
-
-        choose_best_model = BranchPythonOperator(
-            task_id="choose_best_model",
-            python_callable=_choose_best_model
-        )
-
-        mkdirCommand = BashOperator(
+        mkdir_command = BashOperator(
             task_id="mkdirCommand",
-            bash_command="pwd"
+            bash_command='mkdir -p /Users/vikas/app/tmp/' + EXEC_DATE + '/' + source_system_id
         )
 
-        accurate = BashOperator(
-            task_id="accurate",
-            bash_command="echo 'accurate'"
+        sleep_task = BashOperator(
+            task_id="sleep_the_task",
+            bash_command='echo "{{ ti.xcom_push(key="k1", value="vikas.txt") }}"; echo "This is an echo after pushing '
+                         'xcom"',
+            sla=timedelta(seconds=5)
         )
 
-        inaccurate = BashOperator(
-            task_id="inaccurate",
-            bash_command="echo 'inaccurate'"
+        create_file = BashOperator(
+            task_id="createFile",
+            bash_command='touch /Users/vikas/app/tmp/' + EXEC_DATE + '/' + source_system_id + '/"{{ ti.xcom_pull('
+                                                                                              'key="k1") }}" '
         )
+        check_file_task >> mkdir_command >> sleep_task >> create_file
+        return dag
 
-        [training_model_A, training_model_B, training_model_C, mkdirCommand] >> choose_best_model >> [accurate, inaccurate]
+
+# build a dag for each source system
+
+json_file = Variable.get('source_system_id', deserialize_json=True)
+
+source_system_list = json_file['source_system_id']
+
+for source_system_id in source_system_list:
+    dag_id = "dynamic_dag_example_{}".format(str(source_system_id))
+    globals()[dag_id] = create_dag(dag_id, source_system_id)
+
+"""If there is a single task in the file, then we don't need to mention here "check_file_task", 
+thats why its giving warning. We don't even need to assign any variable for the task. """
